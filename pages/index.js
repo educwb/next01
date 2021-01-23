c4cc2a8aea26c07b46afb0d77d4afa2b8a756b70
Eduardo Silva <educwb@gmail.com>
2021-01-23 18:49:59 -0300

    borboleta

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from 'react'
 function Home(){
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Diga quantas vezes a borboletinha que vir para Itanhaém</h1>
       <Contador />
     </div>
   )
