4f4acadedde69d66214c36362759c4e87d4823b5
Eduardo Silva <educwb@gmail.com>
2021-01-24 04:19:00 -0300

    teste do Link para navegação

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
+import Link from 'next/link'
+
 function Home(){
   return (
     <div>
       <h1>Home</h1>
-      <a href="/sobre">Sobre</a>
+
+      <Link href="/sobre">
+        <a>Acessar página Sobre</a>
+      </Link>
     </div>
   )
 }
