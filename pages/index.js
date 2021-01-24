93c6336c79fffcc3110eca991352578b897e7a67
Eduardo Silva <educwb@gmail.com>
2021-01-24 04:03:32 -0300

    delete docx

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,8 @@
-import { useState } from 'react'
-
 function Home(){
   return (
     <div>
-      <h1>Diga quantas vezes a borboletinha quer vir para Itanhaém</h1>
-      <Contador />
-    </div>
-  )
-}
-
-function Contador(){
-  const [contador, setContador] = useState(1)
-
-  function adicionarContador(){
-    setContador(contador + 1)
-  }
-  return (
-    <div>
-      <div>{contador}</div>
-      <button onClick={adicionarContador}>adicionar</button>
+      <h1>Home</h1>
+      <a href="/sobre">Sobre</a>
     </div>
   )
 }
